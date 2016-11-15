e694f493a610d35de88cff28274819274826a585
Justin Molineaux <justin.molineaux@gmail.com>
2016-11-14 22:03:32 -0500

    [Closes #6] Styles upload drop target, displays upload progress.

--- app/assets/javascripts/components/add_contact_dialog.js
+++ app/assets/javascripts/components/add_contact_dialog.js
@@ -1,13 +1,15 @@
 var React          = require('react'),
     mui            = require('material-ui'),
     Dialog         = mui.Dialog,
+    LinearProgress = mui.LinearProgress,
     ContactActions = require('../actions/contact_actions');
 
 var AddContactDialog = React.createClass({
 
   getInitialState : function() {
     return {
-      open : this.props.open
+      open : this.props.open,
+      uploadProgress : 0
     }
   },
 
@@ -23,6 +25,7 @@ var AddContactDialog = React.createClass({
       this.handleFileUploadProgress);
     this.props.contactsStore.addUploadErrorListener(
       this.handleFileUploadError);
+    this.attachJQueryBindings();
   },
 
   componentWillUnmount : function() {
@@ -34,6 +37,17 @@ var AddContactDialog = React.createClass({
       this.handleFileUploadError);
   },
 
+  attachJQueryBindings : function() {
+    $('.add-contact-dialog').on('dragenter', '.upload-target', function(e) {
+      var $uploadTarget = $(e.target).parent('.upload-target');
+      $uploadTarget.find('.upload-target-bg').addClass('file-hovering');
+    });
+    $('.add-contact-dialog').on('dragleave', '.upload-target', function(e) {
+      var $uploadTarget = $(e.target).parent('.upload-target');
+      $uploadTarget.find('.upload-target-bg').removeClass('file-hovering');
+    });
+  },
+
   handleFileUploadCompletion : function() {
     console.log("Woohoo! Upload Comploete");
     ContactActions.getContacts();
@@ -42,7 +56,7 @@ var AddContactDialog = React.createClass({
 
   handleFileUploadProgress : function() {
     var progress = this.props.contactsStore.getUploadProgress();
-    console.log("Uploaded " + progress.loaded + " of " + progress.total);
+    this.setState({uploadProgress: progress.loaded / progress.total * 100 });
   },
 
   handleFileUploadError : function() {
@@ -59,6 +73,7 @@ var AddContactDialog = React.createClass({
     return (
       <Dialog
         title="Add Contacts"
+        className="add-contact-dialog"
         actions={actions}
         modal={false}
         open={this.props.open}
@@ -66,11 +81,23 @@ var AddContactDialog = React.createClass({
         Drag a file on the area below to upload it.
 
         <form>
-          <input
-            type='file'
-            id='fileUpload'
-            onChange={this.handleFileSelectionChanged}/>
+          <div className='upload-target'>
+            <div className='upload-target-bg'>
+              <div className='upload-target-message'>Drop file here!</div>
+            </div>
+            <input
+              type='file'
+              id='fileUpload'
+              className='upload-target-field'
+              onChange={this.handleFileSelectionChanged}/>
+          </div>
         </form>
+
+        <LinearProgress
+          className='upload-progress-bar'
+          mode="determinate"
+          style={{marginTop: '1rem'}}
+          value={this.state.uploadProgress} />
 
       </Dialog>
     );
